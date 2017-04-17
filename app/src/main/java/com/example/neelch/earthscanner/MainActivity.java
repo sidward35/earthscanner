1fa0175f28a07ccec6e9f81143d2e2d5e6bd446b
sidward35 <cherrysid@yahoo.com>
2017-04-16 17:44:58 -0700

    comment out toolbar

--- app/src/main/java/com/example/neelch/earthscanner/MainActivity.java
+++ app/src/main/java/com/example/neelch/earthscanner/MainActivity.java
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
     }
 
     /*
