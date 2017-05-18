6de43e714792cc8b781e9aa373a81bf020f4ce64
Neel Chaudhari <Neel Chaudhari>
2017-05-18 03:21:41 -0700

    add camera

--- app/src/main/java/com/example/mmc/earthscanner/MainActivity.java
+++ app/src/main/java/com/example/mmc/earthscanner/MainActivity.java
@@ -1,13 +1,52 @@
 package com.example.mmc.earthscanner;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    Button b1;
+    ImageView iv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        iv=(ImageView)findViewById(R.id.imageView);
+        Bitmap b = ((BitmapDrawable) iv.getDrawable()).getBitmap();
+        System.out.println(b.getWidth() + " " + b.getHeight());
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
     }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // TODO Auto-generated method stub
+        super.onActivityResult(requestCode, resultCode, data);
+
+        Bitmap bp = (Bitmap) data.getExtras().get("data");
+        iv.setImageBitmap(bp);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
 }
